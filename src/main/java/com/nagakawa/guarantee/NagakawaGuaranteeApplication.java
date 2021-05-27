e85835501b688620c4d792e4fbff7b443093775f
Lê Linh <linh.lehong@evotek.vn>
2021-05-27 17:33:22 +0700

    project initial

---
+++ src/main/java/com/nagakawa/guarantee/NagakawaGuaranteeApplication.java
@@ -0,0 +1,13 @@
+package com.nagakawa.guarantee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NagakawaGuaranteeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NagakawaGuaranteeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nagakawa/guarantee/NagakawaGuaranteeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nagakawa.guarantee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NagakawaGuaranteeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
