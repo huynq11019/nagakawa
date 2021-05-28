d3901ec1c76f7845a0465fcf37b61b03b3f92dc0
Lê Linh <linh.lehong@evotek.vn>
2021-05-28 09:57:02 +0700

    update: Change to maven project

---
+++ src/main/java/com/nagakawa/guarantee/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.nagakawa.guarantee;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(NagakawaGuaranteeApplication.class);
+	}
+
+}
